bdf69217de2786c171565aa9fe2c53dd8d4a9ad6
Juan Consuegra <Consuegra_JCC@hotmail.com>
2021-11-13 19:46:33 -0500

    Imported FlatList, StatusBar, and SafeAreaView

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
-import { StatusBar } from 'expo-status-bar';
+
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {FlatList, StatusBar, SafeAreaView, Modal, ActivityIndicator, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
